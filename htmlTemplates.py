3bb4be32fe7dc87dd979855b0c52e23f5e0e57d2
Ali Zeeshan <70628992+alizeeshan-07@users.noreply.github.com>
2023-07-14 23:27:51 +0500

    Modification

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,7 +28,8 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <!-- <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;"> -->
+        <img src="https://i.ibb.co/ZgTykYW/1.png" alt="1" border="0">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -37,7 +38,7 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
+        <img src="https://i.ibb.co/pnDvbsS/Drawing-Paper-Pencil-Stationery-Logo.png" alt="Drawing-Paper-Pencil-Stationery-Logo" border="0">
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
